628a6f6e7a77a029c9c75f7e145afab0d2e8c871
jwpql <165344621+jwpql@users.noreply.github.com>
2024-09-30 17:16:36 -0400

    1

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function onButtonClick() {
-	alert('button clicked');
-}
+function chara_bg(a)
+{	if (a == 1){
+		document.body.style.backgroundImage; = "img/bg_yuan.jpg";
+	}
+	else if (a == 2){
+		document.body.style.backgroundImage; = "img/bg_mela.jpg";
+	}
 
-const button = document.querySelector('button');
-button.addEventListener('click', onButtonClick);
+};
