404a17b253bc9b1f07a5c95156d91476e59f31b2
jwpql <165344621+jwpql@users.noreply.github.com>
2024-10-01 23:42:44 -0400

    chara bg 4

--- script.js
+++
@@ -1,9 +0,0 @@
-function chara_bg(a)
-{	if (a == 1){
-		document.body.style.backgroundImage; = "img/bg_yuan.jpg";
-	}
-	else if (a == 2){
-		document.body.style.backgroundImage; = "img/bg_mela.jpg";
-	}
-
-};
